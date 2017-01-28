fe8d1cae775be5112d973efe3f13a02c5dab8948
Andrew Volokh <laturo.volokh@yandex.ru>
2017-01-28 19:48:28 +0400

    Update ex.cpp

--- ex.cpp
+++ ex.cpp
@@ -23,12 +23,22 @@ Point::Point(const Point &p){
     cout<<"copy ";
     this->show();
 }
+void Point::show(){
+    cout<<"Point ("<<this->x <<", "<<this->y<<") , color = "<<*(this->color)<<endl;
+}
 Point::~Point(){
     cout<<"delete ";
     this->show();
     delete this->color;
 }
+Point fun(Point p){
+    cout<<"-------\n";
+    p.show();
+    cout<<"---------\n";
+    return p;
+}
 int main() {
     Point p(5,5,10);
     fun(p)
+    Point t=p;
 }
