57d85222cc84160eafbfcc07436b1bfc9adcebbe
DaniilTrofimov <dan-trofimov@mail.ru>
2017-01-28 18:29:30 +0300

    Update ex.cpp

--- ex.cpp
+++ ex.cpp
@@ -1,8 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+class Point{
+    int x,y, *color = new int;
+public:
+    Point(int,int,int);
+    Point(const Point &);
+    void setParam(int,int,int);
+    void show();
+    ~Point();
+};
+
+Point::Point(int x,int y,int color){
+    this->setParam(x,y,color);
+    cout<<"create ";
+    this->show();
+}
+
 Point::Point(const Point &p){
     this->setParam(p.x,p.y,*(p.color));
     cout<<"copy ";
     this->show();
 }
-int main(int argc, const char * argv[]) {
-    
+Point::~Point(){
+    cout<<"delete ";
+    this->show();
+    delete this->color;
+}
+int main() {
+    Point p(5,5,10);
+    fun(p)
 }
