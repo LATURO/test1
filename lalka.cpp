76ed11ff038e6caca0e82d5b3dd77fa869c6df23
Andrew Volokh <laturo.volokh@yandex.ru>
2017-02-04 18:55:39 +0400

    Create lalka.cpp

---
+++ lalka.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class Book{
+private:
+	string name;
+	string autor;
+	unsigned int year;
+	string tema;
+	unsigned int max_adult;
+	unsigned int pages;
+	int rate;
+	int position;
+public:
+	void show();
+	void update_rating();
+	void move(int pos);
+};
+void Book::show(){
+
+}
+void Book::update_rating(){
+
+}
+void Book::move(){
+	
+}
+int main()
+{
+	return 0;
+}
