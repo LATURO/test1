bf992106c08ff4d5503cd335574cdc86d7102fed
Andrew Volokh <laturo.volokh@yandex.ru>
2017-02-04 19:23:01 +0400

    Update lalka.cpp

--- lalka.cpp
+++ lalka.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-
 using namespace std;
-
 class Book{
-private:
+public:
 	string name;
 	string autor;
 	unsigned int year;
@@ -12,77 +10,49 @@ private:
 	unsigned int pages;
 	int rate;
 	int position;
-public:
 	void show();
 	void update_rating();
 	void move(int pos);
 };
 void Book::show(){
-
+	cout<<"Название "<<name<<endl;
+	cout<<"Автор "<<autor<<endl;
+	cout<<"Год Издания "<<year<<"г"<<endl;
+	cout<<"Жанр "<<tema<<endl;
+	cout<<"Ретинг "<<rate<<endl;
+	cout<<"Позиция "<<position<<endl;
 }
 void Book::update_rating(){
+this->rate=rand()%100;
+}
+void Book::move(int pos){
 
 }
-void Book::move(){
-
-}
-
-class libary{
-    Book * books;
-    int book_num;
+class Library
+{
+	Book books;
+	int book_num;
 public:
-    libary();
-    ~libary();
-    void add_book(const book&b);
-    void update_all();
-    void print_all();
+	void show(){
+		books.show();
+	}
+	void add_book(string name,string autor,unsigned int year,string tema,unsigned int max_adult,unsigned int pages,int rate,int position){
+		this->books.name=name;
+		this->books.autor=autor;
+		this->books.year=year;
+		this->books.tema=tema;
+		this->books.max_adult=max_adult;
+		this->books.pages=pages;
+		this->books.rate=rate;
+		this->books.position=position;
+	}
 };
+
 
 int main()
 {
-	return 0;
-}
-#include <iostream>
-
-using namespace std;
-
-class Book{
-private:
-	string name;
-	string autor;
-	unsigned int year;
-	string tema;
-	unsigned int max_adult;
-	unsigned int pages;
-	int rate;
-	int position;
-public:
-	void show();
-	void update_rating();
-	void move(int pos);
-};
-void Book::show(){
-
-}
-void Book::update_rating(){
-
-}
-void Book::move(){
-
-}
-
-class libary{
-    Book * books;
-    int book_num;
-public:
-    libary();
-    ~libary();
-    void add_book(const book&b);
-    void update_all();
-    void print_all();
-};
-
-int main()
-{
+	Library a;
+	a.add_book("a","a",123,"a",123,123,123,123);
+	a.show();
 	return 0;
 }
