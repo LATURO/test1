0cf07e1c5450468c2e6ec1c186d48571c4b4a3a9
DaniilTrofimov <dan-trofimov@mail.ru>
2017-02-04 18:02:45 +0300

    Update lalka.cpp

--- lalka.cpp
+++ lalka.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 using namespace std;
+
 class Book{
 private:
 	string name;
@@ -22,8 +24,64 @@ void Book::update_rating(){
 
 }
 void Book::move(){
-	
+
 }
+
+class libary{
+    Book * books;
+    int book_num;
+public:
+    libary();
+    ~libary();
+    void add_book(const book&b);
+    void update_all();
+    void print_all();
+};
+
+int main()
+{
+	return 0;
+}
+#include <iostream>
+
+using namespace std;
+
+class Book{
+private:
+	string name;
+	string autor;
+	unsigned int year;
+	string tema;
+	unsigned int max_adult;
+	unsigned int pages;
+	int rate;
+	int position;
+public:
+	void show();
+	void update_rating();
+	void move(int pos);
+};
+void Book::show(){
+
+}
+void Book::update_rating(){
+
+}
+void Book::move(){
+
+}
+
+class libary{
+    Book * books;
+    int book_num;
+public:
+    libary();
+    ~libary();
+    void add_book(const book&b);
+    void update_all();
+    void print_all();
+};
+
 int main()
 {
 	return 0;
